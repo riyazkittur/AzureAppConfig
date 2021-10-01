7ef7181762c34b0ccb682aec196de0387e653831
Riyaz Ahmed Kittur <riyaz.ahmed.kittur@kdi.kongsberg.com>
2021-10-01 11:39:16 +0530

    Add project files.

---
+++ AzureAppConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureAppConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
